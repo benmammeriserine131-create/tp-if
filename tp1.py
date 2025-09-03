2791611bbfb631972be80cd418a4d1e2ec13184d
serine <benmammeriserine131@gmail.com>
2025-09-03 16:19:45 +0200

    tp1.py

--- tp1.py
+++ tp1.py
@@ -1,11 +1,4 @@
-# *****Write the Program to Handle User Input
-# name = input("Please enter your name: ")
-# last_name = input("Please enter your last name: ")
 
-# if name and last_name:
-#     print("Welcome", name, last_name)
-# else:
-#     print("Error: You must enter your name and last name.")
 user_name= "admin"
 password= "password123"
 N=input("please enter your user name : ")
