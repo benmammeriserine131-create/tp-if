a54c4b7ddec4f7cef96e4759f472e1a738b16a80
serine <benmammeriserine131@gmail.com>
2025-09-03 16:21:06 +0200

    2.py

---
+++ 2.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: ")
+last_name = input("Please enter your last name: ")
+
+if name and last_name:
+    print("Welcome", name, last_name)
+else:
+    print("Error: You must enter your name and last name.")
